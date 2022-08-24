0acbe14956d38b13e1977d509f275be1ff8e3222
Guy Marshall <guy@cslgroupservices.co.uk>
2022-08-24 12:27:18 +0100

    Remove unnecessary comments + run file reformatter

--- character_counter.py
+++ character_counter.py
@@ -1,7 +1,3 @@
-# take file path as input
-# open file with read
-# iterate through file - line in file, word in line, character in word
-
 file_path = input("Enter file path to file: ")
 file = open(file_path)
 
